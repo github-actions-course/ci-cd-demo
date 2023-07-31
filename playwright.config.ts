94c042b4b0e7417a249ee5df710a044d513bb0a6
alialaa <hello@alialaa.com>
2023-07-31 20:55:36 +0300

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-  reporter: [
-    ["html", { open: 'never'}],
-  ],
+  reporter: [["html", { open: "never" }]],
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:3000",
+  },
 });
