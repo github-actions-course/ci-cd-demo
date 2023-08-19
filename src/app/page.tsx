7eb16af5c4ba7a74dad84580ee09040ca85198b7
Ali Alaa <hello@alialaa.com>
2023-08-19 17:23:33 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
-        <p>new text</p>
+        <p>new text test</p>
       </div>
       <Link href="/about">About</Link>
       <br />
